96683e05773e970264e1b1123a13dec1267aa166
JorellAndreiFinez <jorellandrei23@gmail.com>
2026-01-28 20:35:15 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,16 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
-    }
+     public function register()
+       {
+           if (env('APP_ENV') !== 'local') {
+               URl::forceScheme('https');
+           }
+       }
 
     /**
      * Bootstrap any application services.
